a7ef92e3d14087caf5db243df291aeecbe02f691
Mikhail Vezhdenetskiy <mikhail.vezhdenetskii@marketguru.io>
2024-02-10 03:03:15 +0300

    new files

--- sw_server_oled-128x64_NPN-NO/sw_server_oled-128x64_NPN-NO.ino
+++ sw_server_oled-128x64_NPN-NO/sw_server_oled-128x64_NPN-NO.ino
@@ -29,6 +29,9 @@ GND -> GND
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+// from src path:
+#include "../src/SSID_server.h"
+
 
 #define SCREEN_WIDTH 128     // OLED display width, in pixels
 #define SCREEN_HEIGHT 64     // OLED display height, in pixels
@@ -40,8 +43,8 @@ DNSServer dnsServer;
 const char *server_name = "*"; //"sw";  // Can be "*" to all DNS requests
 
 // Replace with your network credentials
-const char *ssid = "StopWatcher";
-const char *password = "StopWatcher";
+//const char *ssid = "StopWatcher";
+//const char *password = "StopWatcher";
 
 bool ledState = 0;
 const int ledPin = 2;
