1033cae6324194347dbe56431e1dac1352a59332
Mikhail Vezhdenetskiy <muxa@xaker.ru>
2024-06-24 12:17:23 +0300

    Update SSID_server.h

--- sw_server_oled-128x64/SSID_server.h
+++ sw_server_oled-128x64/SSID_server.h
@@ -2,10 +2,10 @@
 
 // Replace with your network credentials
 #ifdef DEFAULT_00
-const char *ssid_name = "StopWatcher";
-const char *ssid_pass = "StopWatcher";
+const char *ssid_name[] = { "StopWatcher" };
+const char *ssid_pass[] = { "StopWatcher" };
 
-// User и паолль для обновелния, через web http://<host>/update
+// User и паолль для обновелния, через web http://<host>/update, examle - http://192.168.4.1/update
 const char *web_user = "admin";
 const char *web_pass = "nimda";
 #endif
