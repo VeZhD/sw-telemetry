1bd01cf19a4d900f489352949be5e58c3ee93e68
Mikhail Vezhdenetskiy <mikhail.vezhdenetskii@marketguru.io>
2024-07-22 12:07:04 +0300

    Greaaaat update!

--- sw_server_oled-128x64/ota_update.h
+++ sw_server_oled-128x64/ota_update.h
@@ -1,18 +1,19 @@
+// User и паолль для обновелния, через web http://<host>/update, examle - http://192.168.4.1/update
+// const char *web_user = "admin";
+// const char *web_pass = "nimda";
 
-#if defined(ESP32)
 
 const char header_html[] PROGMEM = "<!DOCTYPE html><html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'/><meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no'><title>MG StopWatcher - Update!</title></head><body>";
 const char footer_html[] PROGMEM = "</body></html>";
 const char update_html[] PROGMEM = "<h1>Only .bin file</h1><form method='POST' action='/update' enctype='multipart/form-data'><input type='file' accept='.bin' name='update' required><input type='submit' value='Run Update'></form>";
-const String FW_VERSION = "*.*.1";
+const String FW_VERSION = "*.*.*";
 
 
 void OTAWeb_update_begin(){
   // HTTP basic authentication and Simple Firmware Update Form
   server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request){
-    if(!request->authenticate(web_user, web_pass))
-      return request->requestAuthentication();
-  //   request->send(200, "text/html", "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>");
+    // if(!request->authenticate(web_user, web_pass))
+    //   return request->requestAuthentication();
   	String view_html;
 		view_html += header_html;
 		view_html += "<h1>Actual Firmware Release: " + FW_VERSION + "</h1>";
@@ -29,7 +30,6 @@ void OTAWeb_update_begin(){
   },[](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
     if(!index){
       //Serial.printf("Update Start: %s\n", filename.c_str());
-
       if(!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000)){
         Update.printError(Serial);
       }
@@ -64,57 +64,3 @@ void OTAWeb_update_begin(){
 //#endif
 
 }
-
-#endif
-
-
-
-#if defined(ESP8266)
-
-bool shouldReboot = false;
-
-void onUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
-  //Handle upload
-}
-
-void OTAWeb_update_begin(){
-    // upload a file to /upload
-  server.on("/upload", HTTP_POST, [](AsyncWebServerRequest *request){
-    request->send(200);
-  }, onUpload);
-
-
-  // Simple Firmware Update Form
-  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>");
-  });
-  server.on("/update", HTTP_POST, [](AsyncWebServerRequest *request){
-    shouldReboot = !Update.hasError();
-    AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", shouldReboot?"OK":"FAIL");
-    response->addHeader("Connection", "close");
-    request->send(response);
-  },[](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
-    if(!index){
-      Serial.printf("Update Start: %s\n", filename.c_str());
-      Update.runAsync(true);
-      if(!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000)){
-        Update.printError(Serial);
-      }
-    }
-    if(!Update.hasError()){
-      if(Update.write(data, len) != len){
-        Update.printError(Serial);
-      }
-    }
-    if(final){
-      if(Update.end(true)){
-        Serial.printf("Update Success: %uB\n", index+len);
-      } else {
-        Update.printError(Serial);
-      }
-    }
-  });
-
-}
-
-#endif
--- sw_server_oled-128x64/server.h
+++ sw_server_oled-128x64/server.h
@@ -1,91 +1,35 @@
-#ifndef DISPLAY_LASTTIME
-#define DISPLAY_LASTTIME 1
-#endif
-
-#define FONT_PIN      button01
-#define LAST_TIME_PIN button02
-
-bool startStopState;
-bool startStopLastState;
-
-bool SensorState;
-bool SensorLastState;
-
-String startStopStateName;
-
+//#include "WString.h"
 IPAddress myIP;
 String apIP = "";
-uint8_t timerState = 0;
-uint32_t startTime = 0;
-uint32_t currentTime = 0;
-uint32_t lastChange;
-String laptime = "";
 
-bool  HotPlug_State;
-bool  HotPlug_LastState = LOW;
-bool  display_enable;
+// */
 
-bool  button01_State;
-bool  button01_LastState = HIGH;
+/* API is quite simple :
+#include “esp_timer.h”
 
-bool  button02_State;
-bool  button02_LastState = HIGH;
+then call the function
 
-bool  button03_State;
-bool  button03_LastState = HIGH;
+int64_t esp_timer_get_time (void)
 
-uint8_t mode = 0;
-String  modeString = "start-stop";
-
-uint8_t wifi_id = 0;
+*/
 
 bool ssid_state = HIGH;
 bool ssid_laststate = HIGH;
 uint32_t ssid_state_ts =  millis() ;
 
-const uint8_t LastTimeCount = 100;
-uint32_t LastTime[LastTimeCount];
-uint32_t LastCurrentTime = 0;
-uint8_t LastTimeID = 0;
-bool LastTimeID_State = HIGH;
-bool LastTimeID_LastState = HIGH;
-uint32_t LastTimeID_State_ts = millis();
+DNSServer dnsServer;
+const char *server_name = "*"; //"sw";  // Can be "*" to all DNS requests
 
-uint32_t TopTime = 599999;
-uint count = 0;
-
-// JsonDocument config_json;
-// const char* config_filename = "/config.txt";
-
-void CalcTopTime() {
-  if  ((35000 < currentTime) && (currentTime < TopTime) && (currentTime > uint32_t(TopTime / 1.15) or TopTime == 599999) && (ssid_state_ts < millis() - 7500)) {
-    TopTime = currentTime;
-  }
-  count++;
-}
-
-
-void ResetTime() {
-  startTime = 0;
-  currentTime = 0;
-  startStopState = LOW;
-  timerState = LOW;
-  //TimerLastState = LOW;
-  TopTime = 599999;
-  
-  for (int i = LastTimeCount - 1; i >= 0; i--) {
-    LastTime[i] = 0;
-  }
-
-}
-
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
 void connectToHost() {
   WiFi.disconnect(true);
   WiFi.mode(WIFI_OFF);
   delay(100);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid_name[wifi_id], ssid_pass[wifi_id]);
+  WiFi.begin(config["wifi"]["list"][wifi_id]["ssid"].as<String>(), config["wifi"]["list"][wifi_id]["pass"].as<String>());
 }
 
 void StartAPMode() {
@@ -93,183 +37,77 @@ void StartAPMode() {
   WiFi.mode(WIFI_OFF);
   delay(100);
   WiFi.mode(WIFI_AP);
-  // You can remove the password parameter if you want the AP to be open.(ssid, password,channel, hide=1, clients max= )
-  //WiFi.softAP(ssid_name[0], ssid_pass[0], 13, 1, 5);
-  WiFi.softAP(ssid_name[0], ssid_pass[0], 13);
-  //WiFi.softAPConfig(apIP, apIPgate, IPAddress(255, 255, 255, 0));//для кастомного ip, работало почему-то не всегда 
-  //WiFi.softAPConfig(local_IP, gateway, netmask)
+  WiFi.softAP(config["wifi"]["list"][wifi_id]["ssid"].as<String>(), config["wifi"]["list"][wifi_id]["pass"].as<String>(), 13);
+}
 
-  //Это немного магия, но в моем случае это работает так
-  //WiFi.softAP(ssid, password);
-  //delay(2000); // ОЧЕНЬ ВАЖНО
-  //WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+void InitWifi(){
+  if ( wifiMode == "server" ){
+    StartAPMode();
+  } else if ( wifiMode == "client") {
+    connectToHost();
+  }
+}
+
+void InitDNS(){
+    dnsServer.start(53, server_name, IPAddress(192, 168, 4, 1));
 }
 
 void ssidChangeLoop() {
-  //ssid_state = digitalRead(SSID_PIN);
-  //if (ssid_state == LOW && ssid_laststate == HIGH) {
   button02_State = digitalRead(button02);
   button03_State = digitalRead(button03);
   if (button02_State == LOW && button02_LastState != button02_State && button03_State == LOW) {
    // ResetTime();
-    if (wifi_id < (sizeof(ssid_name) / sizeof(char *) - 1) && wifi_id >= 0) {
+    if (wifi_id < wifiList.size() - 1) {
       wifi_id++;
-      delay(250);
-      connectToHost();
     } else {
       wifi_id = 0;
-      delay(150);
-      StartAPMode();
     }
-    EEPROM.write(0, wifi_id);
-    EEPROM.commit();
+    wifiMode = wifiList[wifi_id]["mode"].as<String>();
+    //delay(250);
+    InitWifi();
+    config["wifi"]["wifiid"] = String(wifi_id);
+    saveConfig();
+    // EEPROM.write(0, wifi_id);
+    // EEPROM.commit();
     ssid_state_ts = millis();
     button02_LastState = button01_State;  
     }
   button03_LastState = button03_State; 
-  //ssid_laststate = ssid_state;
 
 }
 
-
-void LastTimeIDChangeLoop() {
-  button02_State = digitalRead(button02);
-  if (button02_State == LOW && button02_LastState == HIGH) {
-    if ( LastTimeID >= 0 && LastTimeID < LastTimeCount - DISPLAY_LASTTIME && LastTime[LastTimeID + DISPLAY_LASTTIME] > 1 ) {
-      LastTimeID++;
-    } else {
-      LastTimeID = 0;
-    }
-    LastTimeID_State_ts = millis();
-  }
-  
-  button02_LastState = button02_State;
-  
-  if (LastTimeID_State_ts + 10500 < millis()) {
-    LastTimeID = 0;
-  }
+void notifyClients(void) {
+  ws.textAll(startStopStateName + String(timerState) + String(currentTime));
 }
 
-void drawDigit(int ddX, int digit) {
-  for (int y = 0; y < SizeY; y++) {
-    for (int x = 0; x < SizeX; x++) {
-      if (digits[Font_ID][digit][y][x] == 1) {
-        display.drawPixel(x + ddX, y, SSD1306_WHITE);
-      } else {
-        display.drawPixel(x + ddX, y, SSD1306_BLACK);       
-      }
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
+  AwsFrameInfo *info = (AwsFrameInfo *)arg;
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+    data[len] = 0;
+    if (strcmp((char *)data, "toggle") == 0) {
+      notifyClients();
     }
   }
 }
 
-void FontChangeLoop() {
-  button01_State = digitalRead(button01);
-  if (button01_State == LOW && button01_LastState == HIGH) {
-    if (Font_ID >= 0 && Font_ID < Font_Count) {
-      Font_ID++;
-    } else {
-      Font_ID = 0;
-    }
-    display.clearDisplay();
-    EEPROM.write(3, Font_ID);
-    EEPROM.commit();
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
+             void *arg, uint8_t *data, size_t len) {
+  switch (type) {
+    case WS_EVT_CONNECT:  //      Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+      break;
+    case WS_EVT_DISCONNECT: //      Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      break;
+    case WS_EVT_DATA:
+      handleWebSocketMessage(arg, data, len);
+      break;
+    case WS_EVT_PONG:
+      break;
+    case WS_EVT_ERROR:
+      break;
   }
-  button01_LastState = button01_State;
 }
 
-
-void TimePrintXY(uint32_t time, byte x, byte y, String name) {
-
-  uint8_t minutes = (int)(time / 60000) % 10;
-  uint8_t tSeconds = (int)(time % 60000 / 10000);
-  uint8_t seconds = (int)(time % 60000 % 10000 / 1000);
-  uint8_t mSeconds = (int)(time % 60000 % 1000 / 100);
-  uint8_t miSeconds = (int)(time % 60000 % 100 / 10);
-  uint8_t milSeconds = (int)(time % 60000 % 10);
-  display.setCursor(x, y); 
-  display.print(name + String(minutes) + ":" + String(tSeconds) + String(seconds) + "." + String(mSeconds) + String(miSeconds) + String(milSeconds));
+void initWebSocket(void) {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
 }
-
-
-void ChangeMode(void) {
-  button01_State = digitalRead(button01);
-  button03_State = digitalRead(button03);
-  if (button01_State == LOW && button01_LastState != button01_State && button03_State == LOW) {
-      if ( mode == 0 ){
-        mode = 1;
-        modeString = "lap-timer";
-      } else {
-        mode = 0;
-        modeString = "start-stop";
-      }
-    button01_LastState = button01_State;  
-    }
-  button03_LastState = button03_State;  
-}
-
-void HotPlug_display(void) {
-  HotPlug_State = digitalRead(HotPlug_pin);
-  if (HotPlug_State == LOW && HotPlug_LastState != HotPlug_State) {
-    delay(450);
-    display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
-    display.clearDisplay();
-    //display.display();
-  }
-
-  HotPlug_LastState = HotPlug_State;  
-}
-
-void TestPressButton01(void) {
-   button01_State = digitalRead(button01);
-  if (button01_State == LOW) {
-  //if (button01_State == LOW && button01_LastState != button01_State) {
-    display.setCursor(0, 40);
-    display.print("1");
-  } else {
-    display.setCursor(0, 40);
-    display.print("#");
-  }
-
-  //button01_LastState = button01_State;  
-}
-
-void TestPressButton02(void) {
-   button02_State = digitalRead(button02);
-  //if (button02_State == LOW && button02_LastState != button02_State) {
-  if (button02_State == LOW) {
-  display.setCursor(6, 40);
-  display.print("2");
-  } else {
-  display.setCursor(6, 40);
-  display.print("#");
-  }
-
-  //button02_LastState = button02_State;  
-}
-
-void TestPressButton03(void) {
-   button03_State = digitalRead(button03);
-  //if (button03_State == LOW && button03_LastState != button03_State) {
-  if (button03_State == LOW) {
-  display.setCursor(12, 40);
-  display.print("3");
-  } else {
-  display.setCursor(12, 40);
-  display.print("#");
-  }
-
-  //button03_LastState = button03_State;  
-}
-
-
-// void LoadConfig(void) {
-//   File file = SPIFFS.open(config_filename);
-
-//   // Deserialize the JSON document
-//   DeserializationError error = deserializeJson(config_json, file);
-//   if (error)
-//     Serial.println(F("Failed to read file, using default configuration"));
-
-
-
-// }
