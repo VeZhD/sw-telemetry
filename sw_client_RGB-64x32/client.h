094b7f16b7ef2c8be621ee13a1effe3b8de50913
Mikhail Vezhdenetskiy <mikhail.vezhdenetskii@marketguru.io>
2024-07-24 19:02:59 +0300

    fix

--- sw_client_RGB-64x32/client.h
+++ sw_client_RGB-64x32/client.h
@@ -10,9 +10,9 @@
 #define SW_BASIC_OTA_HOSTNAME "SW_client"
 #endif
 
-#ifndef SW_BASIC_OTA_PASSWORD
-#define SW_BASIC_OTA_PASSWORD "passwordSW_client"
-#endif
+//#ifndef SW_BASIC_OTA_PASSWORD
+//#define SW_BASIC_OTA_PASSWORD "passwordSW_client"
+//#endif
 
 bool ssid_state = HIGH;
 bool ssid_laststate = HIGH;
@@ -251,7 +251,7 @@ void SW_Basic_OTA() {
   // Hostname defaults to esp3232-[MAC]
   ArduinoOTA.setHostname(SW_BASIC_OTA_HOSTNAME);
   // No authentication by default
-  //ArduinoOTA.setPassword(SW_BASIC_OTA_PASSWORD);
+  //ArduinoOTA.setPassword();
   ArduinoOTA.onStart([]() {
       String type;
       if (ArduinoOTA.getCommand() == U_FLASH)
